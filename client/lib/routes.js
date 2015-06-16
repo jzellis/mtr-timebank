6d8823a037c64deb8508eb1268975e8d1d251293
Joshua Ellis <jzellis@gmail.com>
2015-06-16 16:37:40 -0700

    Fixed stupid error that was stupid

--- client/lib/routes.js
+++ client/lib/routes.js
@@ -19,7 +19,7 @@ Router.route('/', {
     name: "home",
     template: "home",
     subscriptions: function(){
-      if(Meteor.user())
+      // if(Meteor.user())
         return [
         Meteor.subscribe("myFavorites")
         ]
